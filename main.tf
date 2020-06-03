27a03856169ce9aa74c1cae67ebcdd7fdb085304
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-03 18:02:49 -0300

    tf12 upgrade

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_cognito_identity_provider" "example_provider" {
-  user_pool_id      = "${var.user_pool_id}"
-  provider_name     = "${var.provider_name}"
-  provider_type     = "${var.provider_type}"
-  provider_details  = "${var.provider_details}"
-  attribute_mapping = "${var.attribute_mapping}"
+  user_pool_id      = var.user_pool_id
+  provider_name     = var.provider_name
+  provider_type     = var.provider_type
+  provider_details  = var.provider_details
+  attribute_mapping = var.attribute_mapping
 }
+
--- variables.tf
+++ variables.tf
@@ -1,32 +1,33 @@
 variable "user_pool_id" {
-  type        = "string"
+  type        = string
   description = "The user pool id"
 }
 
 variable "provider_name" {
-  type        = "string"
+  type        = string
   description = "The provider name"
 }
 
 variable "provider_type" {
-  type        = "string"
+  type        = string
   description = "The provider type"
 }
 
 variable "attribute_mapping" {
-  type        = "map"
+  type        = map(string)
   description = "The map of attribute mapping of user pool attributes."
   default     = {}
 }
 
 variable "idp_identifiers" {
-  type        = "list"
+  type        = list(string)
   description = "The list of identity providers."
   default     = []
 }
 
 variable "provider_details" {
-  type        = "map"
+  type        = map(string)
   description = "The map of identity details, such as access token"
   default     = {}
 }
+
